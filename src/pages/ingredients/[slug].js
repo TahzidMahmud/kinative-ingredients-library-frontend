17e9d9e7a96bc7cf01675dd282ec1dfc330714d6
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-06-14 21:35:33 +0600

    initial layout

---
+++ src/pages/ingredients/[slug].js
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router'
+import AppLayout from '@/components/Layouts/AppLayout'
+import Head from 'next/head'
+
+const ingredient = props => {
+    const router = useRouter()
+    const { slug } = router.query
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    {slug}
+                </h2>
+            }>
+            <Head>
+                <title>Laravel -{slug}</title>
+            </Head>
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            {slug}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    )
+}
+
+export default ingredient
