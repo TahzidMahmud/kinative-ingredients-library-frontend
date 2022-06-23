bce24ac439e345f2e9796fc65869df35bed8b91c
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-06-23 12:08:08 +0600

    sort by added

--- components/Products/ProductList.js
+++ components/Products/ProductList.js
@@ -54,13 +54,21 @@ const ProductList = ({ categories, products, brands }) => {
       fetchProducts(categories, brands, value);
     }
   }
+  function handleSort(e) {
+    let { value } = e.target;
+    const { brands } = queryparams;
+    const { categories } = queryparams;
 
-  function fetchProducts(q_cats, q_brands, s_q = "") {
+    fetchProducts(categories, brands, "", value);
+  }
+
+  function fetchProducts(q_cats, q_brands, s_q = "", sort_by = "") {
     axios
       .post("/api/filter-products", {
         brands: q_brands.toString(),
         category: q_cats.length > 0 ? q_cats[0] : "",
         s_query: s_q,
+        sort_by: sort_by,
       })
       .then((response) => {
         setFetchProducts(response.data.data);
@@ -79,9 +87,8 @@ const ProductList = ({ categories, products, brands }) => {
           />
         </div>
         <div className="min-w-[75%]">
-          <div className="my-3 ">
-            {/* <form> */}
-            <div className="relative">
+          <div className="my-3 flex">
+            <div className="relative min-w-[75%]">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg
                   className="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -105,14 +112,17 @@ const ProductList = ({ categories, products, brands }) => {
                 placeholder="Search"
                 onChange={handleSearch}
               />
-              <button
-                type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Search
-              </button>
             </div>
-            {/* </form> */}
+            <div className="min-w-[25%]">
+              <select
+                className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={handleSort}
+              >
+                <option value="">Select Sort</option>
+                <option value="desc">Newest to Oldest</option>
+                <option value="asc"> Oldest to Newest</option>
+              </select>
+            </div>
           </div>
           <div className="grid grid-cols-4 gap-2">
             {fetchproducts?.map((product) => (
