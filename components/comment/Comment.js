699b8011af8d2d98d0c81a5ec6a8ceb025aba634
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-07-20 17:42:33 +0600

    reply seperated

--- components/comment/Comment.js
+++ components/comment/Comment.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import axios from "@/lib/axios";
 import CommentForm from "@/components/comment/CommentForm";
-
+import Reply from "@/components/comment/Reply";
 import { useState, useEffect } from "react";
 
 const Comment = ({
@@ -93,7 +93,7 @@ const Comment = ({
     }
   }
   function addReply(comment) {
-    console.log(comment);
+    setReplies([...replies, comment]);
     setCanreply(false);
   }
   return (
@@ -170,52 +170,7 @@ const Comment = ({
       )}
       {/* replies list */}
       {replies.map((reply, index) => (
-        <div key={index} className="flex ">
-          <div className="w-1/12"></div>
-          <div className="w-1/12 border-l border-sky-500 ">
-            <div className="flex justify-center items-center ">
-              <div className="border-dashed border-b border-sky-500 w-[100%] mt-10"></div>
-            </div>
-          </div>
-          <div>
-            <Image
-              src="/avatar.PNG"
-              alt={reply.name}
-              width={60}
-              height={60}
-              className="py-4 my-4"
-            />
-          </div>
-          <div className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-6/12 my-4">
-            <div className="flex mb-3">
-              <span className="opacity-100 font-semibold text-sm mx-2">
-                {reply.autor.name}
-              </span>
-              <span className="opacity-60 text-sm mx-2">
-                {reply.created_at}
-              </span>
-            </div>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {reply.body}
-            </p>
-            <div className="p-3 rounded-lg">
-              {reply.image != null ? (
-                <Image
-                  loader={() => {
-                    return reply.image;
-                  }}
-                  src={reply.image}
-                  alt={reply.autor.name}
-                  width={300}
-                  height={200}
-                  className="py-4"
-                />
-              ) : (
-                <></>
-              )}
-            </div>
-          </div>
-        </div>
+        <Reply key={index} reply={reply}  />
       ))}
     </>
   );
---
+++ components/comment/Reply.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+
+const Reply = ({reply}) => {
+  return (
+    <>
+        <div  className="flex ">
+          <div className="w-1/12"></div>
+          <div className="w-1/12 border-l border-sky-500 ">
+            <div className="flex justify-center items-center ">
+              <div className="border-dashed border-b border-sky-500 w-[100%] mt-10"></div>
+            </div>
+          </div>
+          <div>
+            <Image
+              src="/avatar.PNG"
+              alt={reply.name}
+              width={60}
+              height={60}
+              className="py-4 my-4"
+            />
+          </div>
+          <div className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-6/12 my-4">
+            <div className="flex mb-3">
+              <span className="opacity-100 font-semibold text-sm mx-2">
+                {reply.autor.name}
+              </span>
+              <span className="opacity-60 text-sm mx-2">
+                {reply.created_at}
+              </span>
+            </div>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {reply.body}
+            </p>
+            <div className="p-3 rounded-lg">
+              {reply.image != null ? (
+                <Image
+                  loader={() => {
+                    return reply.image;
+                  }}
+                  src={reply.image}
+                  alt={reply.autor.name}
+                  width={300}
+                  height={200}
+                  className="py-4"
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Reply
