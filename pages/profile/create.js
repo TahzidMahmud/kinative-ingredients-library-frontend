cc7d1e5518a26ca5cc6b89410a19f5a345c30bae
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-07-27 00:25:06 +0600

    profile create page added

--- pages/profile/create.js
+++ pages/profile/create.js
@@ -5,25 +5,62 @@ import GuestLayout from "@/components/Layouts/GuestLayout";
 import Input from "@/components/Input";
 import Label from "@/components/Label";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
+import axios from "../../lib/axios";
 
-const Create = () => {
+const Create = ({ Concerns, SkinTypes }) => {
+  const [user, setUser] = useState(null);
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [stage, setStage] = useState(1);
+  const imageInput = useRef(null);
+  const [cmntimage, setCmntimage] = useState(null);
+
+  const [concerns, setConcerns] = useState([]);
+  const [skinTypes, setSkinTypes] = useState([]);
+
   const [errors, setErrors] = useState([]);
+  if (user == null) {
+    axios
+      .get("/api/user")
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((error) => {
+        if (error.response.status !== 409) throw error;
+      });
+  }
+  function convertImage() {
+    var reader = new FileReader();
+    var url = reader.readAsDataURL(imageInput.current.files[0]);
+    reader.onloadend = function (e) {
+      setCmntimage(reader.result);
+    };
+  }
+  function nextStep() {
+    let prevStage = stage;
+    setStage((prevStage) => prevStage + 1);
+  }
+  function prevStep() {
+    let prevStage = stage;
+    setStage((prevStage) => prevStage - 1);
+  }
   const submitForm = (event) => {
     event.preventDefault();
-
-    register({
-      name,
-      email,
-      password,
-      password_confirmation: passwordConfirmation,
-      setErrors,
-    });
+    let formData = new FormData();
+    formData.append("name", name);
+    formData.append("concerns", JSON.stringify(concerns));
+    formData.append("skinTypes", JSON.stringify(skinTypes));
+    formData.append("image", cmntimage);
+    axios
+      .post("/api/profile", formData)
+      .then((res) => {
+        // setUser(res.data);
+        // setStage(3);
+      })
+      .catch((error) => {
+        setErrors(error.response.data.errors);
+      });
   };
 
   return (
@@ -46,78 +83,195 @@ const Create = () => {
         {/* Validation Errors */}
         <AuthValidationErrors className="mb-4" errors={errors} />
 
-        <form onSubmit={submitForm}>
-          {/* Name */}
-          <div>
-            <Label htmlFor="name">Name</Label>
+        {/* <form onSubmit={submitForm}> */}
+        {/* cover image stage*/}
+        <div
+          className={`${
+            stage == 1 ? "active" : "hidden"
+          } flex flex-col justify-center items-center`}
+        >
+          <Label htmlFor="name" className="text-xl">
+            Upload Profile Image
+          </Label>
 
-            <Input
-              id="name"
-              type="text"
-              value={name}
-              className="block mt-1 w-full"
-              onChange={(event) => setName(event.target.value)}
-              required
-              autoFocus
+          <button
+            type="button"
+            className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            htmlFor="imageInput"
+            onClick={() => imageInput.current.click()}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Upload image</span>
+          </button>
+          <input
+            ref={imageInput}
+            type="file"
+            name="imageInput"
+            multiple="true"
+            onChange={convertImage}
+            className="hidden"
+          />
+        </div>
+        {cmntimage != null && stage == 1 ? (
+          <div className="mx-2 my-2 flex items-center justify-between">
+            <Image
+              loader={() => cmntimage}
+              src={cmntimage}
+              alt={`profile-image`}
+              width={150}
+              height={150}
+              className="py-6 border border-red-400 rounded-lg m-1"
             />
+            <div
+              className="bg-white text-black md:p-4 sm:p-3 h-6 w-6 rounded-2xl flex justify-center items-center"
+              onClick={() => {
+                setCmntimage(null);
+              }}
+            >
+              <span>x</span>
+            </div>
           </div>
+        ) : (
+          <></>
+        )}
 
-          {/* Email Address */}
-          <div className="mt-4">
-            <Label htmlFor="email">Phone</Label>
+        {/* skin type stage */}
+        <div
+          className={`${
+            stage == 2 ? "active" : "hidden"
+          } flex flex-col justify-center items-center`}
+        >
+          <Label htmlFor="name" className="text-xl">
+            Select Skin Type
+          </Label>
 
-            <Input
-              id="email"
-              type="number"
-              value={email}
-              className="block mt-1 w-full"
-              onChange={(event) => setEmail(`${event.target.value}`)}
-              required
-            />
+          <div className="grid grid-cols-2 gap-4 m-6">
+            {SkinTypes.map((skinType, index) => (
+              <div
+                key={index}
+                className={`${
+                  skinTypes.includes(skinType.id) ? ` border-blue-500` : ``
+                }border flex flex-col justify-center items-center`}
+                onClick={() => {
+                  setSkinTypes([...skinTypes, skinType.id]);
+                }}
+              >
+                <Image
+                  loader={() => {
+                    return skinType.image != null
+                      ? skinType.image
+                      : "/avatar.PNG";
+                  }}
+                  src={skinType.image != null ? skinType.image : "/avatar.PNG"}
+                  alt={skinType.name}
+                  width={60}
+                  height={60}
+                  className="py-4"
+                />
+                {skinType.name}
+              </div>
+            ))}
           </div>
+        </div>
 
-          {/* Password */}
-          <div className="mt-4">
-            <Label htmlFor="password">Password</Label>
+        {/* concern stage */}
+        <div
+          className={`${
+            stage == 3 ? "active" : "hidden"
+          } flex flex-col justify-center items-center`}
+        >
+          <Label htmlFor="name" className="text-xl">
+            Select Concern
+          </Label>
 
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              className="block mt-1 w-full"
-              onChange={(event) => setPassword(event.target.value)}
-              required
-              autoComplete="new-password"
-            />
+          <div className="grid grid-cols-2 gap-4 m-6">
+            {Concerns.map((concern, index) => (
+              <div
+                key={index}
+                className={`${
+                  concerns.includes(concern.id) ? ` border-blue-500` : ``
+                }  border flex flex-col justify-center items-center`}
+                onClick={() => {
+                  setConcerns([...concerns, concern.id]);
+                }}
+              >
+                <Image
+                  loader={() => {
+                    return concern.image != null
+                      ? concern.image
+                      : "/avatar.PNG";
+                  }}
+                  src={concern.image != null ? concern.image : "/avatar.PNG"}
+                  alt={concern.name}
+                  width={60}
+                  height={60}
+                  className="py-4"
+                />
+                {concern.name}
+              </div>
+            ))}
           </div>
+        </div>
 
-          {/* Confirm Password */}
-          <div className="mt-4">
-            <Label htmlFor="passwordConfirmation">Confirm Password</Label>
-
-            <Input
-              id="passwordConfirmation"
-              type="password"
-              value={passwordConfirmation}
-              className="block mt-1 w-full"
-              onChange={(event) => setPasswordConfirmation(event.target.value)}
-              required
-            />
-          </div>
-
+        {stage !== 3 ? (
           <div className="flex items-center justify-end mt-4">
-            <Link href="/login">
-              <a className="underline text-sm text-gray-600 hover:text-gray-900">
-                Already registered?
-              </a>
-            </Link>
-
-            <Button className="ml-4">Register</Button>
+            {stage !== 1 ? (
+              <Button className="ml-4" onClick={prevStep}>
+                Previous
+              </Button>
+            ) : (
+              <></>
+            )}
+            <Button className="ml-4" onClick={nextStep}>
+              Next
+            </Button>
           </div>
-        </form>
+        ) : (
+          <div className="flex items-center justify-end mt-4">
+            <Button className="ml-4" onClick={prevStep}>
+              Previous
+            </Button>
+            <Button className="ml-4">Submit</Button>
+          </div>
+        )}
+        {/* </form> */}
       </AuthCard>
     </GuestLayout>
   );
 };
+
+export async function getServerSideProps(context) {
+  const concerns = await axios
+    .get("/api/concerns")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+  const skinTypes = await axios
+    .get("/api/skin-types")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
+  return {
+    props: {
+      Concerns: concerns.data,
+      SkinTypes: skinTypes.data,
+    },
+  };
+}
 
 export default Create;
