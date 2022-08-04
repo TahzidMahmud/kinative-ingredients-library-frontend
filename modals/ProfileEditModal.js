1c367f0f548374451a8d85f593f13f74a31780f8
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-08-04 22:10:50 +0600

    profile edit started

---
+++ modals/ProfileEditModal.js
@@ -0,0 +1,357 @@
+import Link from "next/link";
+import Input from "@/components/Input";
+import { useEffect, useState } from "react";
+import Label from "@/components/Label";
+import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "../lib/axios";
+
+const ProfileEditModal = ({
+  show,
+  closeModal,
+  profile,
+  Concerns,
+  Skintypes,
+}) => {
+  const [name, setName] = useState(profile.user.name);
+  const [email, setEmail] = useState(profile.user.email);
+  const [phone, setPhone] = useState(profile.user.phone);
+  const [concerns, setConcerns] = useState([profile.concern.data[0].id]);
+  const [skinTypes, setSkinTypes] = useState([profile.skin_type.data[0].id]);
+  const [datechange, setDatechange] = useState(null);
+
+  const [dateValues, timeValues] = profile.birth_day.split(" ");
+  const [month, day, year] = dateValues.split("/");
+  const [hours, minutes, seconds] = timeValues.split(":");
+  const [birthtDate, setBirthtDate] = useState(
+    new Date(+year, month - 1, +day, +hours, +minutes, +seconds)
+  );
+  const cur_date = new Date();
+  const years = date_range(1990, cur_date.getFullYear() + 1, 1);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [errors, setErrors] = useState([]);
+  function date_range(start, end, step) {
+    let years = [];
+    for (let i = start; i <= end; i += step) {
+      years.push(i);
+    }
+    return years;
+  }
+  useEffect(() => {
+    var aScript1 = document.createElement("script");
+    aScript1.type = "text/javascript";
+    aScript1.src = "https://cdn.tailwindcss.com";
+    document.head.appendChild(aScript1);
+    var aScript = document.createElement("script");
+    aScript.type = "text/javascript";
+    aScript.src =
+      "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js";
+    document.head.appendChild(aScript);
+    aScript.onload = () => {};
+  }, []);
+
+  function submitForm(event) {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("userid", user.id);
+    formData.append("concerns", JSON.stringify(concerns));
+    formData.append("skinTypes", JSON.stringify(skinTypes));
+    formData.append("birth_date", Math.floor(birthtDate.getTime() / 1000));
+    formData.append("image", cmntimage);
+    formData.append("imgname", imageInput.current.value);
+    axios
+      .post("/api/profiles", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        if (res.data.success) {
+          closeModal();
+          location.reload();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <div className="">
+      <div
+        className={`${
+          show ? "active" : "hidden"
+        }  bg-black w-screen h-screen opacity-40 absolute top-0 left-0 z-10`}
+      ></div>
+      <div
+        id="authentication-modal"
+        tabIndex="-1"
+        className={`${
+          show ? "active" : "hidden"
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center`}
+        aria-modal="true"
+        role="dialog"
+      >
+        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+          <div className="relative bg-white rounded-lg shadow ">
+            <button
+              type="button"
+              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+              data-modal-toggle="authentication-modal"
+              onClick={closeModal}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="py-6 px-6 lg:px-8">
+              <div className="p-4">
+                <h3 className="text-lg font-bold text-gray-900 text-center p-1">
+                  Edit Profile
+                </h3>
+                <hr></hr>
+                <div className="h-96 overflow-y-auto ">
+                  <form className="space-y-6" action="#">
+                    <div>
+                      <label className="block mb-2 text-md font-semibold pt-1 text-gray-900">
+                        Your Name
+                      </label>
+                      <Input
+                        type="text"
+                        value={name}
+                        className="block mt-1 w-full"
+                        onChange={(event) => setName(event.target.value)}
+                        required
+                        autoFocus
+                      />
+                    </div>
+                    <div>
+                      <label className="block mb-2 text-md font-semibold pt-1 text-gray-900">
+                        Your Email
+                      </label>
+                      <Input
+                        type="text"
+                        value={email}
+                        className="block mt-1 w-full"
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                        autoFocus
+                      />
+                    </div>
+                    <div>
+                      <label className="block mb-2 text-md font-semibold pt-1 text-gray-900">
+                        Your Phone
+                      </label>
+                      <Input
+                        type="text"
+                        value={phone}
+                        className="block mt-1 w-full"
+                        onChange={(event) => setPhone(event.target.value)}
+                        required
+                        autoFocus
+                      />
+                    </div>
+                    {/* skin type stage */}
+                    <div
+                      className={`flex flex-col justify-center items-center`}
+                    >
+                      <label className="block mb-2 text-md font-semibold pt-1 text-gray-900">
+                        Skin Type
+                      </label>
+
+                      <div className="grid grid-cols-1 gap-4 m-6 w-full">
+                        <select
+                          value={skinTypes[0]}
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        >
+                          {Skintypes.map((skinType, index) => (
+                            <option
+                              key={index}
+                              value={skinType.id}
+                              onClick={() => {
+                                setSkinTypes([skinType.id]);
+                              }}
+                              className="text-cetner"
+                            >
+                              {skinType.name}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    </div>
+
+                    {/* concern stage */}
+                    <div
+                      className={` flex flex-col justify-center items-center`}
+                    >
+                      <label className="block mb-2 text-md font-semibold pt-1 text-gray-900">
+                        Concerns
+                      </label>
+
+                      <div className="grid grid-cols-2 gap-4 m-6">
+                        {Concerns.map((concern, index) => (
+                          <div
+                            key={index}
+                            className={`${
+                              concerns.includes(concern.id)
+                                ? `border-2 border-blue-500 rounded-xl `
+                                : ``
+                            }  border flex flex-col justify-center items-center p-3`}
+                            onClick={() => {
+                              setConcerns([...concerns, concern.id]);
+                            }}
+                          >
+                            {concern.name}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div
+                      className={`flex flex-col justify-center items-center`}
+                    >
+                      <Label htmlFor="name" className="text-xl">
+                        Select Birth Date
+                      </Label>
+                      <div className="flex items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ">
+                        <div
+                          className="inset-y-0 left-0 items-center pl-3 pointer-events-none -ml-3"
+                          htmlFor="dd"
+                          onClick={() => {
+                            const elem = document.getElementsByClassName(
+                              "react-datepicker-wrapper"
+                            );
+                            elem[0].click();
+                            elem.click();
+                          }}
+                        >
+                          {" "}
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        </div>
+                        <DatePicker
+                          renderCustomHeader={({
+                            date,
+                            changeYear,
+                            changeMonth,
+                            decreaseMonth,
+                            increaseMonth,
+                            prevMonthButtonDisabled,
+                            nextMonthButtonDisabled,
+                          }) => (
+                            <div
+                              style={{
+                                margin: 10,
+                                display: "flex",
+                                justifyContent: "center",
+                              }}
+                            >
+                              <button
+                                onClick={decreaseMonth}
+                                disabled={prevMonthButtonDisabled}
+                                className="p-1 m-2"
+                              >
+                                {"<"}
+                              </button>
+                              <select
+                                value={date.getFullYear()}
+                                onChange={({ target: { value } }) =>
+                                  changeYear(value)
+                                }
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              >
+                                {years.map((option) => (
+                                  <option key={option} value={option}>
+                                    {option}
+                                  </option>
+                                ))}
+                              </select>
+
+                              <select
+                                value={months[date.getMonth()]}
+                                onChange={({ target: { value } }) =>
+                                  changeMonth(months.indexOf(value))
+                                }
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              >
+                                {months.map((option) => (
+                                  <option key={option} value={option}>
+                                    {option}
+                                  </option>
+                                ))}
+                              </select>
+
+                              <button
+                                onClick={increaseMonth}
+                                disabled={nextMonthButtonDisabled}
+                                className="p-1 m-2"
+                              >
+                                {">"}
+                              </button>
+                            </div>
+                          )}
+                          selected={birthtDate}
+                          onChange={(date) => {
+                            setBirthtDate(date);
+                            setDatechange(true);
+                          }}
+                          dateFormat="dd/MM/yyyy"
+                          onClick={() => console.log("clicked")}
+                          className="bg-gray-50 border-none text-gray-900 sm:text-sm rounded-lg  block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+                        />
+                      </div>
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                      onClick={submitForm}
+                    >
+                      Update Profile
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEditModal;
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -2,11 +2,16 @@ import axios from "@/lib/axios";
 import Image from "next/image";
 import { useAuth } from "@/hooks/auth";
 import AppLayout from "@/components/Layouts/AppLayout";
+import ProfileEditModal from "@/modals/ProfileEditModal";
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
-const Profile = ({ Profile }) => {
+const Profile = ({ Concerns, SkinTypes, Profile }) => {
+  const [editProfile, setEditProfile] = useState(false);
+  function closeModal() {
+    setEditProfile(false);
+  }
   return (
     <AppLayout
       header={
@@ -18,7 +23,141 @@ const Profile = ({ Profile }) => {
       <Head>
         <title>Laravel </title>
       </Head>
-      ;
+
+      <div className="flex items-center p-10">
+        {/* left section  */}
+        <div className="md:min-w-[30%] px-2 border-r">
+          <div className="flex flex-col items-center justify-center">
+            {/* image  */}
+            <div>
+              <Image
+                loader={() => Profile.avatar}
+                src={Profile.avatar}
+                alt={Profile.user.name}
+                width={350}
+                height={350}
+                className="py-6 border border-blue-500 rounded-sm"
+              />
+            </div>
+            {/* details  */}
+            <div></div>
+          </div>
+        </div>
+        {/* right section  */}
+        <div className="md:min-w-[70%] px-6">
+          <div className="p-6 w-full h-96 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
+            <div className="flex justify-between imtes-center border-b pb-2">
+              <div className="flex items-center ">
+                <Image
+                  src="/user.gif"
+                  alt={Profile.user.name}
+                  width={70}
+                  height={70}
+                  className="py-2"
+                />
+                <span className="text-xl font-semibold px-1">About</span>
+                {Profile.rank !== null ? (
+                  <button className="rounded-full bg-emerald-300 p-3 text-white text-sm mx-2">
+                    {Profile.rank}
+                  </button>
+                ) : (
+                  <button className="rounded-full bg-red-600 p-3 text-white text-sm mx-2">
+                    Un-Ranked
+                  </button>
+                )}
+              </div>
+
+              <div
+                className=""
+                onClick={() => {
+                  console.log("edit profile");
+                  setEditProfile(true);
+                }}
+              >
+                <div className="p-4 bg-stone-100 rounded-full">
+                  <Image
+                    src="/edit.svg"
+                    alt={Profile.user.name}
+                    width={20}
+                    height={20}
+                    className=""
+                  />
+                </div>
+              </div>
+            </div>
+            {/* profile Description */}
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <div className="grid grid-cols-2 py-6 md:px-4 sm:px-3">
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Name
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.user.name}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Skin Type
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.skin_type.data[0].name}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Email
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.user.email != null ? Profile.user.email : "Not Set"}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Points
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.points}
+                </div>
+              </div>
+              <div className="grid grid-cols-2 py-6">
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Birthday
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.birth_date}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Concern
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.concern.data.map((con, index) => {
+                    return <div key={index}>{con.name}</div>;
+                  })}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Contact No
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.user.phone}
+                </div>
+                <div className="font-semibold text-md md:my-3 sm:my-2">
+                  Member Since
+                </div>
+                <div className=" text-md opacity-70 md:my-3 sm:my-2">
+                  {Profile.created_at}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {editProfile ? (
+          <div className="h-2/3">
+            <ProfileEditModal
+              show={editProfile}
+              closeModal={closeModal}
+              profile={Profile}
+              Concerns={Concerns}
+              Skintypes={SkinTypes}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </AppLayout>
   );
 };
@@ -31,8 +170,23 @@ export async function getServerSideProps(context) {
       return response.data;
     })
     .catch((error) => console.log(error));
+  const concerns = await axios
+    .get("/api/concerns")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+  const skinTypes = await axios
+    .get("/api/skin-types")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
   return {
     props: {
+      Concerns: concerns.data,
+      SkinTypes: skinTypes.data,
       Profile: profile.data,
     },
   };
