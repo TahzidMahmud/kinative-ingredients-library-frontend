788ccf76a31298c9d0212def258d2e6a4bfdbaef
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-08-10 21:30:21 +0600

    event design initial done

--- pages/events/index.js
+++ pages/events/index.js
@@ -64,7 +64,7 @@ const Events = ({ events, winners }) => {
           </Link>
         ))}
       </div>
-      <div className="grid grid-flow-col grid-cols-10 grid-rows-1 my-10">
+      <div className="grid grid-flow-col grid-cols-10 grid-rows-1 my-10 pb-10">
         <div className="col-span-2 flex flex-col justify-start items-start">
           <h1 className="text-xl font-bold my-4">Event winners</h1>
           <div className="flex flex-col w-full">
@@ -90,7 +90,86 @@ const Events = ({ events, winners }) => {
             ))}
           </div>
         </div>
-        <div className="col-span-10">fuck</div>
+        <div className="col-span-10 pl-10">
+          <h1 className="text-xl font-bold my-4">Win Points to Participate</h1>
+          <div className="flex flex-col grid grid-cols-5 gap-4 w-full">
+            <div className="bg-white shadow-sm sm:rounded-lg p-4 my-2 w-full">
+              <div className="flex  flex-col items-center">
+                <Image
+                  src="/post.PNG"
+                  alt={`post`}
+                  width={150}
+                  height={200}
+                  className=""
+                />
+                <span className="text-sm my-2 font-medium mx-4 opacity-70">
+                  Write a Post and Get
+                </span>
+                <span className="text-md font-semibold mx-4">20 points</span>
+              </div>
+            </div>
+            <div className="bg-white shadow-sm sm:rounded-lg p-4 my-2 w-full">
+              <div className="flex  flex-col items-center">
+                <Image
+                  src="/post.PNG"
+                  alt={`post`}
+                  width={150}
+                  height={200}
+                  className=""
+                />
+                <span className="text-sm my-2 font-medium mx-4 opacity-70">
+                  Like a Comment and Get
+                </span>
+                <span className="text-md font-semibold mx-4">1 point</span>
+              </div>
+            </div>
+            <div className="bg-white shadow-sm sm:rounded-lg p-4 my-2 w-full">
+              <div className="flex  flex-col items-center">
+                <Image
+                  src="/post.PNG"
+                  alt={`post`}
+                  width={150}
+                  height={200}
+                  className=""
+                />
+                <span className="text-sm my-2 font-medium mx-4 opacity-70">
+                  Write a Post and Get
+                </span>
+                <span className="text-md font-semibold mx-4">20 points</span>
+              </div>
+            </div>
+            <div className="bg-white shadow-sm sm:rounded-lg p-4 my-2 w-full">
+              <div className="flex  flex-col items-center">
+                <Image
+                  src="/profile.PNG"
+                  alt={`post`}
+                  width={150}
+                  height={200}
+                  className=""
+                />
+                <span className="text-sm my-2 font-medium mx-4 opacity-70">
+                  Create Account and Get
+                </span>
+                <span className="text-md font-semibold mx-4">500 points</span>
+              </div>
+            </div>
+            <div className="bg-white shadow-sm sm:rounded-lg p-4 my-2 w-full">
+              <div className="flex  flex-col items-center">
+                <Image
+                  src="/review.PNG"
+                  alt={`post`}
+                  width={150}
+                  height={200}
+                  className=""
+                />
+                <span className="text-sm my-2 font-medium mx-4 opacity-70">
+                  Write a Review and Get
+                </span>
+                <span className="text-md font-semibold mx-4">50 points</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </AppLayout>
   );
