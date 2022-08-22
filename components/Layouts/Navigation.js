e1d7855cb9b1aa79c66417b805b960138511f768
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2022-08-22 19:17:47 +0600

    responsive nav hamburger added

--- components/Layouts/Navigation.js
+++ components/Layouts/Navigation.js
@@ -157,34 +157,15 @@ const Navigation = ({ user }) => {
           </div>
 
           {/* Hamburger */}
-          <div className="-mr-2 flex items-center sm:hidden">
+          <div className="mr-2 flex items-center md:hidden">
             <button
               onClick={() => setOpen((open) => !open)}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
             >
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                {open ? (
-                  <path
-                    className="inline-flex"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    className="inline-flex"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
+              <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="50" height="5"></rect>
+                <rect y="15" width="50" height="5"></rect>
+                <rect y="30" width="50" height="5"></rect>
               </svg>
             </button>
           </div>
